9a1881bacbed175257bf0b449c55611d75d207cb
raxit-parmar <raxit.parmar@linearloop.io>
2024-02-13 11:38:00 +0530

    code commit

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AboutPage from "./aboutPage";
+
+const About = () => {
+  return (
+    <>
+      <div>about</div>
+      <AboutPage />
+    </>
+  );
+};
+
+export default About;
